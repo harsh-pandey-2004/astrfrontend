13c0bb46579c17d360052386c4dd6b6033d712ef
coderHarsh121212 <codesource287@gmail.com>
2024-05-23 15:21:10 +0530

    harsh
    Signed-off-by: coderHarsh121212 <codesource287@gmail.com>

--- src/Pages/BookPandit.js
+++ src/Pages/BookPandit.js
@@ -23,26 +23,6 @@ const BookPandit = (props) => {
       <p className="text-xl text-center font-sans font-semibold text-[#f1d980] md:text-2xl">
         Get Pandit filter by price location and type of Pooja
       </p>
-      <div className="flex flex-col  items-center justify-center w-screen  ">
-        <div className="flex items-center px-12  w-full mt-2">
-          <div className="h-px w-full bg-[#f1d980] "></div>
-          <img src="https://aws.astrotalk.com/assets/images/astrotalk-mini-logo.webp" />
-          <div className="h-px w-full bg-[#f1d980] "></div>
-        </div>
-      </div>
-      <TrendingPooja/>
-      <Stat amount="10,500" live="711" customers="45.5" type="pandit"/>
-      <Testimonials/>
-     
-
-
-     <Link to="/panditfilter" className="flex justify-center items-center">
-
-               <button className=" border  bg-yellow-400  rounded-md max-w-fit mx-auto px-6 py-2  font-semibold hover:shadow-lg transition text-gray-700 hover:text-black mt-6" >Book Now</button>
-     
-     </Link>
-              
-      
     </div>
   );
 };
