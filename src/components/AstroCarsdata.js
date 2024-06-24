99924054927d08fef85305b0f63e2782b5dcb8fc
Aishwary <aishwary3103@gmail.com>
2024-06-24 16:41:58 +0530

    Links

--- src/components/AstroCarsdata.js
+++ src/components/AstroCarsdata.js
@@ -2,27 +2,32 @@ const AstroCarsdata=[
     {
         name:"Prateek",
         img:"https://d1gcna0o0ldu5v.cloudfront.net/fit-in/78x78/consultant_pic/p-38160.jpg",
-        desc:"Vedic Astrology"
+        desc:"Vedic Astrology",
+        exp:"9"
     },
     {
         name:"Mridul",
         img:"	https://d1gcna0o0ldu5v.cloudfront.net/fit-in/78x78/consultant_pic/p-39334.jpg",
-        desc:"Tarot Reading"
+        desc:"Tarot Reading",
+         exp:"2"
     },
     {
         name:"Deepika",
         img:"https://d1gcna0o0ldu5v.cloudfront.net/fit-in/78x78/consultant_pic/p-37603.jpg",
-        desc:"Palm-Reading"
+        desc:"Palm-Reading",
+         exp:"1"
     },
     {
         name:"Shipra",
         img:"https://d1gcna0o0ldu5v.cloudfront.net/fit-in/78x78/consultant_pic/p-37782.jpg",
-        desc:"Vedic Astrology"
+        desc:"Vedic Astrology",
+         exp:"3"
     },
     {
         name:"Anand",
         img:"https://aws.astrotalk.com/consultant_pic/p-42745.jpg",
-        desc:"Tarot Reading"
+        desc:"Tarot Reading",
+         exp:"3"
     }
 ];
 export default AstroCarsdata;
