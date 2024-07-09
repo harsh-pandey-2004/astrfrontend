4ede58dbb27b4825c2f2588d21214dbc5873da5b
Aishwary <aishwary3103@gmail.com>
2024-07-09 17:50:40 +0530

    ReadPlanets

--- src/components/kundalicomponents/KnowMore/ReadPlanets.js
+++ src/components/kundalicomponents/KnowMore/ReadPlanets.js
@@ -1,38 +1,83 @@
 import React from 'react'
 import ReadPlanetHead from './ReadPlanetHead'
+import Vedio8 from "../../../vedios/vedio8.mp4";
+import { GoDotFill } from "react-icons/go";
+
 
 const ReadPlanets = () => {
   return (
     <div className=' mb-12'>
      <ReadPlanetHead/>
-     <div className='pt-12'>
+     <div className='pt-16'>
         <h1 className='text-2xl font-semibold text-center '>How to Interpret Planetary Positions in Astrology</h1>
         <p className='py-6 px-20'>In portion 1 of <span className='text-yellow-600'>how to examined kundli</span>, we have learned the importance of kundli whereas moreover learning a thing or two around planets, zodiac signs and houses in kundli. Nowadays, in portion 2 of the arrangement, we are going learn in detail around planets in soothsaying, what they cruel and how to studied them when attempting to examined your kundli on your possess.
-Planets, you must know, are the foremost fundamental component of crystal gazing. They are the planets whose development from one house to another influences your life occasions. This happens since as planets move from one house to another, they too bring their significance or properties to that specific house.
+Planets, you must know, are the foremost fundamental component of crystal gazing. They are the planets whose development from one house to another influences your life occasions. This happens since as planets move from one house to another, they too bring their significance  to that specific house.
  </p>
      </div>
 
-
-   
-
-     <div >
+     <div className='pt-6'>
      <h1 className='text-2xl font-semibold text-center'>Benefic and Malefic aspects of Planets</h1>
 
-     <div className='flex items-center'>
+     <div className='flex items-center '>
         <div className='w-1/2'>
      
-     <p className='px-20 pt-6'>All the 9 planets in crystal gazing are categorised beneath 2 categories.. </p>
+     <p className='px-20 pt-8'>All the 9 planets in crystal gazing are categorised beneath 2 categories.. </p>
      <p className='px-20 '><span className='font-semibold'>Benefic planets in astrology - </span> <span className='text-yellow-600'>Moon, Mercury, Venus and Jupiter</span></p>
      <p className='px-20 '><span className='font-semibold'>Malefic planets in astrology - </span> <span className='text-yellow-600'>Sun, Mars, Saturn, Rahu and Ketu</span></p>
      <p className='px-20'>The benefic planets are the ones that generally provide positive comes about to the local. On the other hand, the malefic planets are known for their awful or hurtful impacts. Be that as it may, at different occasions, indeed a benefic planet can allow negative comes about to the local. For illustration:
-     in case we consider the planet Moon, Full Moon, i.e. the Moon close Purnima, is considered benefic. In any case, on the other hand, No Moon, i.e. Moon close Amavasya, is considered as a malefic one by the crystal gazers. </p>
+     in case we consider the planet Moon, Full Moon, i.e. the Moon close Purnima, is considered benefic.  </p>
      </div>
 
-      <div className='w-1/2 outline'>
-      <img src=""/>
-      </div>
+      
+      <div className=' mt-4 mr-20 md:w-1/2 xs:w-full   rounded-md  '>
+       <video  autoPlay loop muted className='pt-6 rounded-md  '>
+              <source src={Vedio8} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+
+       </div>
+      
       </div>
      </div>
+
+
+     <div className='pt-12'>
+      <h1 className='text-2xl font-semibold text-center'>Each planet's companion and foe planet in crystal gazing </h1>
+      <p className='py-6 px-20'>Like we have specified over, the positive and negative impact of a planet in a house too depends on the reality whether the planet is set within the house with an foe or inviting zodiac sign or planet.For case:
+the planet Sun is sweet friends with the Aries sign. Additionally, the Sun is additionally companions with Damages, Moon and Jupiter. So at whatever point the Sun is coming with these signs of zodiac in a house, the comes about for the local are most likely to be positive. So also, Sun's adversary sign is Libra, and subsequently their conjunction might not come as productive for the local.
+Each planet moreover has its Claim sign, aka, the sign which it rules over. For case, Sun's possess sign is Leo. So Sun and Leo's conjunction can as well come out to be productive for the local.
+Any planet gives positive comes about in lifted up sign (uchcha rashi), possess sign (swa-rashi), and inviting sign (mitra-rashi). In the interim, negative comes about are when the planet is balanced with weakened sign (neecha-rashi) or enemy's sign (shatru-rashi), it isn't able of giving great comes about. </p>
+     </div>
+
+  
+
+     <div >
+     <p className='pl-20'>The table below highlights a planet’s exalted, debilitated and own sign:</p>
+     <div className='px-20 flex flex-col gap-4 mt-6'>
+        <p className='flex items-center  gap-16 font-semibold'> <span className='w-16 '>No</span> <span className='w-32 '>Planet</span> <span className='w-32 '>Exalted Sign</span> <span className='w-32 '> Debilitated Sign</span> <span className='w-32 '>	Own Sign</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >1</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >2</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >3</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >4</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >5</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >6</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >7</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >8</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+        <p className='flex items-center gap-16 '> <span className='w-16 ' >9</span> <span className='w-32 '>Sun</span> <span className='w-32 '>Aries</span> <span className='w-32 '> Libra</span> <span className='w-32 '>	Leo</span>		 	 </p>
+     </div>
+    
+     </div>
+
+
+     <div>
+      <p className='px-20 mt-6'>To examined a kundli, you would like to be exceptionally well mindful of the neighborly and enemy signs of distinctive planets. Well, there's a trick to memorize the table by heart in case you're incapable to retain it. All you wish to do is keep in mind these three things:</p>
+       <p className='flex items-center gap-2 px-20 mt-3 '><GoDotFill /> <span>To review the over table at the time of require,  lifted up and weakened signs of a planet are continuously seventh from each other. 
+        </span></p>
+      <p className='flex items-center gap-2 px-20 mt-3 '><GoDotFill /><span>Within the over table, take note that Rahu and Ketu are not masters of any of the signs </span></p>
+      <p className='flex items-center gap-2 px-20 mt-3 '><GoDotFill /><span>In the mean time, the Sun and Moon are the rulers of as it were one sign each. All other planets have two signs that they run the show over. </span></p>
+     </div>
+
+
     </div>
   )
 }
