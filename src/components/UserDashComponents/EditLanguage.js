2888c3a9e2b6b01c3c633281198c88b02cc8b9b3
coderHarsh121212 <codesource287@gmail.com>
2024-08-24 22:24:23 +0530

    new

--- src/components/UserDashComponents/EditLanguage.js
+++ src/components/UserDashComponents/EditLanguage.js
@@ -40,7 +40,7 @@ const EditLanguage = ({arr,setArr}) => {
     <div className='h-3/4 w-1/2  mx-auto  bg-white rounded-md border border-black'>
         <div className='w-full bg-black text-white py-2 text-center rounded-t-md'>Select Languages You Know</div>
         <div className='flex flex-wrap  items-center gap-3 pt-6 px-4'>
-            {Languages.map((obj)=>{return <button className='rounded-md px-3 py-1 bg-yellow-400 text-white ' onClick={LangHandler(arr,obj)}>{obj}</button>})}
+            {Languages.map((obj)=>{return <button className='rounded-md px-3 py-1 bg-yellow-400 text-white ' onClick={()=>LangHandler(arr,obj)}>{obj}</button>})}
         </div>
         
 </div>
